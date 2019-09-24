40352209a7903276b47de87389a8ca2109a9d0b9
Adam Zielinski <adam.zielinski@kurier.at>
2019-09-24 19:58:28 +0200

    Argument Capture

--- src/Rpg.hs
+++ src/Rpg.hs
@@ -19,14 +19,14 @@ newCharacter :: Character
 newCharacter = Character {health = 1000, level = 1, state = Alive}
 
 damage :: (Character, Character, Natural) -> Character
-damage (Character {state = Alive}, Character {health = def_h, level = def_l, state = Alive}, amount) =
+damage (Character {state = Alive}, defender@Character {health = def_h, state = Alive}, amount) =
   if amount >= def_h
-    then Character {health = 0, level = def_l, state = Dead}
-    else Character {health = def_h - amount, level = def_l, state = Alive}
+    then defender {health = 0, state = Dead}
+    else defender {health = def_h - amount, state = Alive}
 
 heal :: (Character, Character, Natural) -> Character
-heal (Character {state = Alive}, Character {health = h, level = l, state = Alive}, amount) =
-  Character {health = min 1000 (amount + h), level = l, state = Alive}
+heal (Character {state = Alive}, blessed@Character {health = h, state = Alive}, amount) =
+  blessed {health = min 1000 (amount + h)}
 heal (Character {state = Dead}, _, _) = error "A dead character cannot heal"
 heal (_, Character {state = Dead}, _) = error "A dead character cannot be healed"
 
